7ee3ea716fd13f4bcdb0856db869d23abe05eeb7
Rui Gil <ruipgil@gmail.com>
2016-05-30 13:30:25 +0100

    Fix db schema and add db initialization

--- schema.sql
+++ schema.sql
@@ -1,39 +1,5 @@
-CREATE EXTENSION postgis;
-SELECT postgis_full_version();
-
-CREATE TYPE _timestamp as TIMESTAMP WITHOUT TIME ZONE
-CREATE TYPE _bounds as geography(POLYGON, 4326)
-CREATE TYPE _line as geography(POINTLINE, 4326)
-
-CREATE TABLE IF NOT EXISTS trips (
-  trip_id SERIAL PRIMARY KEY,
-
-  start_location location_id NULL,
-  end_location location_id NULL,
-
-  start_date _timestamp NOT NULL,
-  end_date _timestamp NOT NULL
-
-  bounds _bounds NOT NULL,
-  points _line NOT NULL,
-  -- Length of timestamps must be the same as the lenght of points
-  timestamps _timestamp[] NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS trips_transportation_modes (
-  mode_id SERIAL PRIMARY KEY,
-  trip_id SERIAL REFERENCES trips(trip_id) NOT NULL,
-
-  label TEXT NOT NULL,
-
-  start_date _timestamp NOT NULL,
-  end_date _timestamp NOT NULL,
-
-  -- Indexes of Trips(point/timestamp)
-  start_index INTEGER NOT NULL,
-  end_index INTEGER NOT NULL,
-  bounds _bounds NOT NULL
-);
+-- CREATE EXTENSION postgis;
+-- SELECT postgis_full_version();
 
 CREATE TABLE IF NOT EXISTS locations (
   label TEXT PRIMARY KEY,
@@ -43,10 +9,55 @@ CREATE TABLE IF NOT EXISTS locations (
   point_cluster geography(MULTIPOINT, 4326) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS trips (
+  trip_id SERIAL PRIMARY KEY,
+
+  start_location TEXT REFERENCES locations(label),
+  end_location TEXT REFERENCES locations(label),
+
+  start_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  end_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+
+  bounds geography(POLYGON, 4326) NOT NULL,
+  points geography(LINESTRING, 4326) NOT NULL,
+  -- Length of timestamps must be the same as the lenght of points
+  timestamps TIMESTAMP WITHOUT TIME ZONE[] NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS trips_transportation_modes (
+  mode_id SERIAL PRIMARY KEY,
+  trip_id SERIAL REFERENCES trips(trip_id) NOT NULL,
+
+  label TEXT NOT NULL,
+
+  start_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  end_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+
+  -- Indexes of Trips(point/timestamp)
+  start_index INTEGER NOT NULL,
+  end_index INTEGER NOT NULL,
+  bounds geography(POLYGON, 4326) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS stays (
   stay_id SERIAL PRIMARY KEY,
-  trip_id SERIAL REFERENCES trips(trip_id)
-  location_label TEXT REFERENCES location(label),
+  trip_id SERIAL REFERENCES trips(trip_id),
+  location_label TEXT REFERENCES locations(label),
   start_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   end_date TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS canonical_trips (
+  canonical_id SERIAL PRIMARY KEY,
+
+  start_location TEXT NOT NULL,
+  end_location TEXT NOT NULL,
+
+  bounds geography(POLYGON, 4326) NOT NULL,
+  points geography(LINESTRING, 4326) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS canonical_trips_relations (
+  canonical_trip SERIAL NOT NULL,
+  trip SERIAL NOT NULL
+  );
---
+++ start_db.sh
@@ -0,0 +1,15 @@
+PG_DOCKER_PORT=5432
+docker stop pms-postgis
+docker rm pms-postgis
+echo "Starting container"
+docker run --name pms-postgis -p $PG_DOCKER_PORT -e POSTGRES_USER=pmsteps -e POSTGRES_PASSWORD=pmsteps -d mdillon/postgis
+
+echo "Getting container port"
+PMS_DOCKER_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "'$PG_DOCKER_PORT'/tcp") 0).HostPort}}' pms-postgis)
+
+
+echo "Initializing schema"
+env PGPASSWORD=pmsteps psql -h localhost -p $PMS_DOCKER_PORT -d pmsteps -U pmsteps -w -f schema.sql
+
+echo "Container port"
+echo $PMS_DOCKER_PORT
